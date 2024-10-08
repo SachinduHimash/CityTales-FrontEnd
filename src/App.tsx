40613c3c5f8b8039b5a7cf26bcacccb4de114fa3
SachinduHimash <himash1997@gmail.com>
2024-10-08 18:59:37 +0200

    connect with backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import ARView from './components/ARView'
+import ARView from './components/ARView';
 
 function App() {
   return (
--- src/components/ARView.tsx
+++ src/components/ARView.tsx
@@ -1,12 +1,86 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import 'aframe';
 import 'ar.js';
+import axios from 'axios';
+import { Input, Button, Image, Upload, GetProp, UploadFile, UploadProps } from 'antd';
+import { PlusOutlined, UploadOutlined } from '@ant-design/icons';
+
+
+
+
+type FileType = Parameters<GetProp<UploadProps, 'beforeUpload'>>[0];
+
+const getBase64 = (file: FileType): Promise<string> =>
+    new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result as string);
+      reader.onerror = (error) => reject(error);
+    });
+
+const { TextArea } = Input;
 
 export default function ARView() {
-    return (
-        <div>
-            Hi
-        </div>
-     );
+  const [story, setStory] = useState('');
+  const [previewOpen, setPreviewOpen] = useState(false);
+  const [previewImage, setPreviewImage] = useState('');
+  const [fileList, setFileList] = useState<UploadFile[]>([]);
+
+  console.log(process.env.REACT_APP_BASE_URL)
+
+  const handlePreview = async (file: UploadFile) => {
+    if (!file.url && !file.preview) {
+      file.preview = await getBase64(file.originFileObj as FileType);
+    }
+
+    setPreviewImage(file.url || (file.preview as string));
+    setPreviewOpen(true);
+  };
+  const handleChange: UploadProps['onChange'] = ({ fileList: newFileList }) =>
+    setFileList(newFileList);
+
+  const uploadButton = (
+    <button style={{ border: 0, background: 'none' }} type="button">
+      <PlusOutlined />
+      <div style={{ marginTop: 8 }}>Upload</div>
+    </button>
+  );
+
+  const handleCreate = async () => {
+    try {
+      await axios.post(`${process.env.REACT_APP_BASE_URL}/upload`, { story });
+      alert('Data created successfully!');
+      // Optionally, fetch and update the displayed data
+    } catch (error) {
+      console.error('Error creating data:', error);
+    }
+  };
+
+  return (
+    <div>
+      <Upload
+        action="https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload"
+        listType="picture-card"
+        fileList={fileList}
+        onPreview={handlePreview}
+        onChange={handleChange}
+      >
+        {fileList.length >= 1 ? null : uploadButton}
+      </Upload>
+      {previewImage && (
+        <Image
+          wrapperStyle={{ display: 'none' }}
+          preview={{
+            visible: previewOpen,
+            onVisibleChange: (visible) => setPreviewOpen(visible),
+            afterOpenChange: (visible) => !visible && setPreviewImage(''),
+          }}
+          src={previewImage}
+        />
+      )}
+       <TextArea onChange={(e)=>{setStory(e.target.value)}} rows={4} />
+       <Button icon={<UploadOutlined />} onClick={handleCreate} type="primary">Upload</Button>
+    </div>
+  );
 }
 
